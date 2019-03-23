6aacbecafbeeb78bb18e0e76b0eea88cd43f8ce0
Calypso Blue <46878997+Avalineai@users.noreply.github.com>
2019-03-23 15:29:31 -0700

    mostly finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,89 @@
-var number = 60;
+$(document).ready(function () {
 
-var intervalId;
+    var number = 90;
 
-var correct = 0;
-var incorrect = 0;
+    var intervalId;
+    var correct = 0;
+    var incorrect = 0;
 
-var ansArr = ["fruit", "bruss" , "dragon", "purple", "square", "star", "asp"]
+    var ansArr = ["fruit", "bruss", "dragon", "purple", "square", "star", "asp"]
 
-$("#triviaquiz").hide();
-$("#endgame-score").hide();
-
-$("button").on("click", function () {
-    $("#triviaquiz").show();
-    $("button").hide();
-});
-
-function run() {
-    intervalId = setInterval(decrement, 1000);
-}
-function decrement() {
-    number--;
-    $("#time-rem").text("Time Remaining: " + number);
-    if (number === 0) {
-        stop();
-
-        // alert("Time Up!");
-    }
-}
-function stop() {
-    clearInterval(intervalId);
     $("#triviaquiz").hide();
-    $("#endgame-score").show();
-}
+    $("#endgame-score").hide();
 
-run();
+    $("#startbutton").on("click", function () {
+        $("form").css("display", "block");
+        $("#time-rem").css("display", "block");
+        $("#triviaquiz").show();
+        $("#startbutton").hide();
+    });
 
-ansArr.forEach(function(inpName) {
-$('input[name=' + inpName + ']').on('click', function () {
-    var formans = $(this);
-    if (formans.val() == 'ans') {
-        correct++; //move this to a check answers function, and to a "done" button function.
-        $("input[name=" + inpName + "]").attr('disabled', true);
-        console.log("correct: "+ correct)
-    } else {
-        incorrect++;
-        $("input[name=" + inpName + "]").attr('disabled', true);
-        console.log("wrong: " + incorrect)
+    function timeConverter(t) {
+        var minutes = Math.floor(t / 60);
+        var seconds = t - (minutes * 60);
+        if (seconds < 10) {
+        seconds = "0" + seconds;
+        }
+        if (minutes === 0) {
+        minutes = "00";
+        }
+        else if (minutes < 10) {
+        minutes = "0" + minutes;
+        }
+        return minutes + ":" + seconds;
     }
 
-$("#correct-ans").text("Correct: " + correct)
-$("#incorrect-ans").text("Incorrect: " + incorrect)
-})
+    function run() {
+        intervalId = setInterval(decrement, 1000);}
+    function decrement() {
+        number--;
+        $("#time-rem").html("Time Remaining: "+ timeConverter(number));
+        if (number === 0) {
+            ansArr.forEach(function (inpName) {
+                var formans = $("input[name=" + inpName + "]:checked").val()
+                if (formans == 'ans') {
+                    correct++;
+                    console.log("correct: " + correct)
+                } else {
+                    incorrect++;
+                    console.log("wrong: " + incorrect)
+                }
+                $("#donebutton").hide();
+                stop();
+                $("#correct-ans").text("Correct: " + correct)
+                $("#incorrect-ans").text("Incorrect: " + incorrect)
+            })
+            stop();
+        }
+
+    }
+    function stop() {
+        clearInterval(intervalId);
+        $("#triviaquiz").hide();
+        $("#endgame-score").show();
+    }
+
+    run();
+
+    $("#donebutton").on("click", function (event) {
+        event.preventDefault();
+        ansArr.forEach(function (inpName) {
+            var formans = $("input[name=" + inpName + "]:checked").val()
+            if (formans == 'ans') {
+                correct++;
+                console.log("correct: " + correct)
+            } else {
+                incorrect++;
+                console.log("wrong: " + incorrect)
+            }
+            $("#donebutton").hide();
+            stop();
+            $("#correct-ans").text("Correct: " + correct)
+            $("#incorrect-ans").text("Incorrect: " + incorrect)
+        })
+
+    });
+        
+        
+
 })
